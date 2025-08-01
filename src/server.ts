61335751a0811d35e934418b406dfec315fba95e
moawedmohamed <moawedmohamed144@gmail.com>
2025-08-01 20:23:23 +0300

    add two files for prod and dev

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express"
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello in the Home Page</h1><br><p>Welcome !</p>');
+})
+const PORT: number = 5000
+app.listen(PORT, () => {
+    console.log(`the server is running in port ${PORT}`);
+
+}) 
